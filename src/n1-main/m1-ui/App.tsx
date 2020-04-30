7d36895eb5ef9898a7d666f84c841e1eada8e254
Andrei Mitskevich <metro.trend.now@gmail.com>
2020-04-30 23:43:33 +0300

    init commit

---
+++ src/n1-main/m1-ui/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './App.css';
+
+const App =()=> {
+
+    return (
+    <div className="App">
+      {/*//hr,prov*/}
+<>
+  {/*<Header>*/}
+  {/*<Main>*/}
+
+
+</>
+    </div>
+  );
+}
+
+export default App;
